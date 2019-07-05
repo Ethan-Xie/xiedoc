3f72305b888a4a4f6e404a05c236dbab6d2d22eb
xiethan <xiethan@qq.com>
2019-07-05 23:17:20 +0800

    添加了 随机字符类

--- server/application/api/controller/Item.php
+++ server/application/api/controller/Item.php
@@ -293,7 +293,11 @@ class Item extends Base
   public function test()
   {
     Session_start();
-    $this->sendResult(['name' => session_name(), 'id' => session_id()]);
+    // $this->sendResult(['name' => session_name(), 'id' => session_id()]);
+    $res = sprintf(
+        '%04x%04x-%04x-%04x-%04x-%04x%04x%04x', mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000, mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+    );
+    $this->sendResult(['name' => session_name(), 'id' => session_id(), 'uuid' => $res]);
   }
 
 }
